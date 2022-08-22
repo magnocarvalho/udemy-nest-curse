875f3bc87682a586bccfcd2a2cdbb08d916a877b
Magno Carvalho <magno.carvalho@dorotech.com.br>
2022-08-21 22:33:15 -0300

    feat: atuh init

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { JogadoresModule } from "./jogadores/jogadores.module";
 import { MongooseModule } from "@nestjs/mongoose";
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
 	imports: [
@@ -13,6 +14,7 @@ import { MongooseModule } from "@nestjs/mongoose";
 			useUnifiedTopology: true,
 		}),
 		JogadoresModule,
+		CategoriasModule,
 	],
 	controllers: [AppController],
 	providers: [AppService],
