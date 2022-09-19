4c32bc1439de475cd6bebee7a90f04233cd3a4d1
Magneira <magno.santos@u4crypto.com>
2022-09-18 22:08:54 -0300

    add port dinamic

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	const config = new DocumentBuilder().setTitle("Magneira API").setDescription("The UDEMY CURSE API description").setVersion("1.0").build();
 	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup("doc", app, document);
+	SwaggerModule.setup("docs", app, document);
 	app.useGlobalFilters(new AllExceptionsFilter());
-	await app.listen(3000);
+	await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
